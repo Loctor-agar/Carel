4cbb8ab42adacbf760613b807fdeadf39804a5f6
AlexWalker <alexwolk28@gmail.com>
2017-01-06 16:45:40 +0600

    Add few tasks

--- app/src/main/java/kg/kloop/rinat/newcarel/MainActivity.java
+++ app/src/main/java/kg/kloop/rinat/newcarel/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 /**
- * Этот класс предназначен для указания команд Карелу */
+ * Этот класс предназначен для указания команд Карелу
+ */
 public class MainActivity extends AppCompatActivity {
-    
+
     private TextView textView;
     private CarelGrid grid;
     private GameCanvas canvas;
@@ -21,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
         canvas = new GameCanvas(textView);
-        grid = new CarelGrid(6 , 4); // здесь изменяется размер поля (x,y)
+        grid = new CarelGrid(6, 4); // здесь изменяется размер поля (x,y)
         carel = new Carel(canvas, grid);
 
 
@@ -32,14 +34,19 @@ public class MainActivity extends AppCompatActivity {
         * 1) Поворот направо
         * 2) Разворот
         * 3) Дойти до стены и не умереть
-        * 4) Заполнить поле биперами по периметру
-        * 5) Заполнить всё поле биперами (убрав те, которые уже есть),
+        * 4) Вернуться на начальную клетку из любой другой (Карел смотрит на восток)
+        * 5) Заполнить поле биперами по периметру
+        * 6) Шахматная доска из биперов
+        * 7) Заполнить всё поле биперами (убрав те, которые уже есть),
         * причем в каждой следующей клетке на 1 бипер больше, чем в предыдущей
-        * 6) На начальной клетке выложить сумму всех биперов на поле
-        * 7) Бипер в центре поля
-        * 8) Буква "А" из биперов
-        * 9) Спираль из биперов
-        * 10) Выход из лабиринта
+        * 8) Переложить биперы на 1 клетку вперед
+        * 9) На начальной клетке выложить сумму (*произведение) всех биперов на поле
+        * 10) Подсчет количества клеток до произвольного бипера
+        * 11) Бипер в центре поля
+        * 12) Буква "А" из биперов
+        * 13) Спираль из биперов
+        * 14) Квадрат из биперов с возможностью задать размеры
+        * 15) Выход из лабиринта
         * */
 
         //Ниже идут команды Карелу.
@@ -48,40 +55,37 @@ public class MainActivity extends AppCompatActivity {
         carel.turnLeft();
 
 
-
         //Команды Карелу закончились.
 
     }
-    
-    
+
+
     //Здесь пишем новые методы.
-    
 
 
-    
     //Новые методы закончились.
-    
-    private void move(){
+
+    private void move() {
         carel.move();
     }
-    
-    private void turnLeft(){
+
+    private void turnLeft() {
         carel.turnLeft();
     }
-    
-    private void collectBeeper(){
+
+    private void collectBeeper() {
         carel.collectBeeper();
     }
-    
-    private void dropBeeper(){
+
+    private void dropBeeper() {
         carel.dropBeeper();
     }
-    
-    private boolean isFrontClear(){
+
+    private boolean isFrontClear() {
         return carel.isFrontClear();
     }
-    
-    private boolean isBeeper(){
+
+    private boolean isBeeper() {
         return carel.isBeeper();
     }
 
